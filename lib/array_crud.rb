3eaf3fa21e1acc409d2b61b62f7221ec105bdc18
Ryan Locascio <loc.fitness1@gmail.com>
2020-05-05 13:51:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  number = [1,2,3,4]
-  numbers << 5
+  array = [1,2,3,4]
+  element = wow
+  array << wow 
 end
 
 def add_element_to_start_of_array(array, element)
