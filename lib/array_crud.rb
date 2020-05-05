1e94cf0ccfed93581b4cc09a19f1605b005ab2f2
Ryan Locascio <loc.fitness1@gmail.com>
2020-05-05 14:23:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,35 +3,40 @@ def create_an_empty_array
 end
 
 def create_an_array
-  numbers = ["1", "2", "3", "4"]
+  ["lil' bub", "grumpy cat", "Maru"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["1", "2", "3", "4"]
-  element = "wow"
-  element.push("wow")
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  element.push("nala cat")
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  element.unshift("nala cat")
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  maru_cat = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  lil_bub = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+   array = ["lil' bub", "grumpy cat", "Maru"]
+   array[1]
 end
 
 def retrieve_first_element_from_array(array)
-
+   array = ["lil' bub", "grumpy cat", "Maru"]
+   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["lil' bub", "grumpy cat", "Maru"]
+  array[2]
 end
